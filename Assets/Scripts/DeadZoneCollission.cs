68e53af4e2c08e30c7fe6cb6cd80963ea454c157
Christopher Borger <chrisborger@Christophers-MacBook-Pro.local>
2017-11-02 10:31:30 +0100

    handle fly out off the screen

---
+++ Assets/Scripts/DeadZoneCollission.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadZoneCollission : MonoBehaviour {
+
+	public GameObject respawnPrefab;
+	private GameObject taxi;
+
+	void Update() {		
+		taxi = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.collider.tag == "Player") {
+			Destroy(taxi);
+			Instantiate(respawnPrefab, new Vector3 (respawnPrefab.transform.position.x, respawnPrefab.transform.position.y,0), Quaternion.identity);
+		}
+
+	}
+}
