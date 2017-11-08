7b84f7c18f1b9cf1f3520b469bcd50890f6e0a38
Georgios Dimitriadis <gedi@gedimbp.local>
2017-11-08 07:58:08 +0100

    Optimized collision script

--- Assets/Scripts/DeadZoneCollission.cs
+++ Assets/Scripts/DeadZoneCollission.cs
@@ -6,13 +6,9 @@ public class DeadZoneCollission : MonoBehaviour {
 	public GameObject respawnPrefab;
 	private GameObject taxi;
 
-	void Update() {		
-		taxi = GameObject.FindGameObjectWithTag ("Player");
-	}
-
 	void OnCollisionEnter2D(Collision2D col) {
 		if (col.collider.tag == "Player") {
-			Destroy(taxi);
+			Destroy(GameObject.FindGameObjectWithTag (col.collider.tag));
 			Instantiate(respawnPrefab, new Vector3 (respawnPrefab.transform.position.x, respawnPrefab.transform.position.y,0), Quaternion.identity);
 		}
 
