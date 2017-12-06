4c03254f90da85650ba6f60ce04d1df3ae3f7101
Georgios Dimitriadis <gedi@gedimbp.local>
2017-12-06 12:33:56 +0100

    inital work with player health

---
+++ Assets/Scripts/CustomerSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpawner : MonoBehaviour {
+	public Transform[] spawnPoints;
+	public GameObject customer;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(customer, new Vector3 (spawnPoints[2].position.x, spawnPoints[2].position.y, 0), Quaternion.identity);
+	}
+	
+	void FixedUpdate () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+	public float maxHealth;
+	public GameObject explosionFX;
+
+	private float currentHealth;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (currentHealth <= 0) {
+			destroyPlayer ();
+		}
+	}
+
+	public void destroyPlayer() {
+		Instantiate (explosionFX, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+
+	public void addDamage(float damage) {
+		currentHealth -= damage;
+	}
+}
