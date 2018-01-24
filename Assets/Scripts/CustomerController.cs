3ff2caa8b0e33f7bfba7a3c6684ce4ebf89907ab
Christopher Borger <christopherborger@Christophers-MacBook-Pro.local>
2018-01-24 12:54:57 +0100

    added customer controller

---
+++ Assets/Scripts/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerController : MonoBehaviour {
+
+	private string currentTag = "";
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D other) {
+		
+		int layer = other.gameObject.layer;
+
+		if (layer == LayerMask.NameToLayer ("Platform")) {
+			currentTag = other.gameObject.tag;
+
+			Debug.Log ("Player on platform: " + currentTag);
+		}
+	}
+}
