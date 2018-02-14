1c3caa050d1fe903e0ce02f690efa96e1ca417e0
Georgios Dimitriadis <gedi@gedimbp.local>
2018-02-14 12:14:58 +0100

    cleanup and start of spawning

--- Assets/Scripts/CustomerSpawner.cs
+++ Assets/Scripts/CustomerSpawner.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class CustomerSpawner : MonoBehaviour {
 	public Transform[] spawnPoints;
 	public GameObject customer;
+	public int maxCustomers;
+	private int currentCustomers;
 
 	// Use this for initialization
 	void Start () {
-		Instantiate(customer, new Vector3 (spawnPoints[2].position.x, spawnPoints[2].position.y, 0), Quaternion.identity);
+		currentCustomers = 0;
+		this.Spawn ();
 	}
 	
 	void FixedUpdate () {
-		
+		if (currentCustomers <= maxCustomers) {
+			Spawn ();
+		}
+	}
+
+	void Spawn ()
+	{
+		// Find a random index between zero and one less than the number of spawn points.
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+		Instantiate (customer, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		currentCustomers++;
+	}
+
+	public void removeCustomer() {
+		currentCustomers--;
 	}
 }
